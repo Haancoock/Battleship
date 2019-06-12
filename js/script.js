2cc6450038b6a7cf42f572bfa025b213d52e555e
alim <haancoock123@gmail.com>
2019-06-12 23:34:18 +0400

    set all variables

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 var Module = (function(){
+	// Все необходимые переменные 
+	var inputName = document.querySelector('.name-input'),// Инпут ввода Имени
+		infoBar = document.querySelector('.infobar-text'), // Информационная строка сверху
+		restartButton = document.querySelector('.restart'); // Кнопка для рестарта игры 
+		startButton = document.getElementById('start'), // Кнопка старта игры
+		cheatButton = document.getElementById('cheater') // Кнопка для отображения кораблей компа
+		compFieldId = '#compField', // Айди обертки поля боя компьютера и игрока
+		playerFieldId = '#playerField', // нужно для удобной работы с селекторами jQuery
+		fields = document.querySelectorAll('.battlefield-grid'); // Массив из оберток двух полей  
 
+	
 	return {
 		gameStart: gameStart
 	}
